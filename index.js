276d602d638769a1678a07b1ab940746f42c09ca
Wellington Gallo <wellinton.gallo77@gmail.com>
2021-01-22 13:56:00 -0300

    Add bootstrap

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function verifyWord() {
     text.split(" ").map(item => {
         if (item.trim().length > textBig.length) {
             textBig = item.trim();
-            document.getElementById('bigWord').innerHTML = item.trim();
+            document.getElementById('bigWord').innerHTML = `The biggest word in the sentence is: ${item.trim()}`;
         }
     });
 }
